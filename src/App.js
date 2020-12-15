1ef3a81569fdb2eb49197d65ac7541ce0df2a680
Nathan Steed <steedn@outlook.com>
2020-12-14 18:47:34 -0800

    started adding react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
-function App() {
+
+
+class App extends React.Component {
+  handleClick = event => {
+    fetch('http://localhost:3000/fuels')
+      .then(res => res.json())
+      .then(json => console.log(json)) 
+  }
+  render(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    
+    // debugger
+    <div>
+          <button onClick={this.handleClick}>Click to Fetch!</button>
     </div>
-  );
+  );}
 }
 
 export default App;
